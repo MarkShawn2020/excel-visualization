9877229551b363d716491c015730c0aca424f312
markshawn2020 <shawninjuly@gmail.com>
2023-08-02 03:04:13 +0800

    update

--- src/components/deck.gl/index.tsx
+++ src/components/deck.gl/index.tsx
@@ -9,16 +9,59 @@ import { INITIAL_VIEW_STATE } from '@/lib/mapconfig'
 import { Columns, ColumnType, mapBox } from '@/config'
 import { LanguageControl } from '@/components/deck.gl/controls/language.control'
 import { DrawControl } from '@/components/deck.gl/controls/draw.control'
-import { DataFilterExtension } from '@deck.gl/extensions'
-import { ScatterplotLayer } from '@deck.gl/layers'
 import data from '../../../data/table.json'
 import { Select, SelectContent, SelectGroup, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table'
 import { useStoreBear } from '@/store'
 import { Slider } from '@/components/ui/slider'
 
+import { ScatterplotLayer } from '@deck.gl/layers'
+import { HexagonLayer } from '@deck.gl/aggregation-layers'
 
-const LocationAggregatorMap = () => {
+import { DataFilterExtension } from '@deck.gl/extensions'
+import { AmbientLight, LightingEffect, PointLight } from '@deck.gl/core'
+
+export const colorRange = [
+	[1, 152, 189],
+	[73, 227, 206],
+	[216, 254, 181],
+	[254, 237, 177],
+	[254, 173, 84],
+	[209, 55, 78],
+]
+
+const material = {
+	ambient: 0.64,
+	diffuse: 0.6,
+	shininess: 32,
+	specularColor: [51, 51, 51],
+}
+
+const ambientLight = new AmbientLight({
+	color: [255, 255, 255],
+	intensity: 1.0,
+})
+
+const pointLight1 = new PointLight({
+	color: [255, 255, 255],
+	intensity: 0.8,
+	position: [-0.144528, 49.739968, 80000],
+})
+
+const pointLight2 = new PointLight({
+	color: [255, 255, 255],
+	intensity: 0.8,
+	position: [-3.807751, 54.104682, 8000],
+})
+
+const lightingEffect = new LightingEffect({ ambientLight, pointLight1, pointLight2 })
+
+
+const LocationAggregatorMap = ({
+	                               radius = 1000,
+	                               upperPercentile = 100,
+	                               coverage = 1,
+                               }) => {
 	const { col, range, setColumn, setRangeValue, setRangeScope } = useStoreBear()
 	
 	const dataFilter = new DataFilterExtension({ filterSize: 1, fp64: false })
@@ -47,7 +90,20 @@ const LocationAggregatorMap = () => {
 		pickable: true,
 	})
 	
-	// console.log({ filter })
+	const hexagonLayer = new HexagonLayer({
+		id: 'heatmap',
+		colorRange,
+		coverage,
+		data,
+		elevationRange: [0, 3000],
+		elevationScale: data && data.length ? 50 : 0,
+		extruded: true,
+		getPosition: d => d.coordinates,
+		pickable: true,
+		radius,
+		upperPercentile,
+		material,
+	})
 	
 	
 	return (
@@ -55,11 +111,13 @@ const LocationAggregatorMap = () => {
 			<DeckGL
 				layers={[
 					scatterLayer,
+					// hexagonLayer,
 					// iconLayer,
 				]}
+				effects={[lightingEffect]}
 				initialViewState={INITIAL_VIEW_STATE}
 				controller={{ dragRotate: false }}
-				getTooltip={({ object }) => object && {
+				getTooltip={({ object }) => object && console.log(object) && {
 					html: Object.entries(object).map(([key, val]) => (
 						`<div>${key}: ${val}</div>`
 					)).join(''),
