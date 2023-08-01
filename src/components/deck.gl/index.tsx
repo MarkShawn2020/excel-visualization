acddb1573d4f46e191138d70bf93c0f09811b847
markshawn2020 <shawninjuly@gmail.com>
2023-08-02 00:41:55 +0800

    :rocket: added range filter

--- src/components/deck.gl/index.tsx
+++ src/components/deck.gl/index.tsx
@@ -1,26 +1,79 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 
 import Map from 'react-map-gl'
-import DeckGL from '@deck.gl/react/typed'
+import DeckGL from '@deck.gl/react'
 
 import { INITIAL_VIEW_STATE } from '@/lib/mapconfig'
 import { mapBox } from '@/config'
-import { iconLayer } from '@/components/deck.gl/layers/icon.layer'
 import { LanguageControl } from '@/components/deck.gl/controls/language.control'
 import { DrawControl } from '@/components/deck.gl/controls/draw.control'
+import { DataFilterExtension } from '@deck.gl/extensions'
+import { ScatterplotLayer } from '@deck.gl/layers'
+import data from '../../../data/table.json'
+import { Slider } from '@/components/ui/slider'
+import { SliderRange } from '@radix-ui/react-slider'
+
+export const VALUE_RANGE = [0, 200]
+
+export const RangeInput = ({ setRange }: {
+	setRange: (values: number[]) => void
+}) => {
+	return (
+		<div className={'absolute inset-4 rounded-md p-4 w-80 h-40 bg-cyan-800 flex justify-center items-center text-slate-200 whitespace-nowrap'}>
+			<div className={'flex w-full gap-2'}>
+				<p>Range({VALUE_RANGE.join(', ')}): </p>
+				<Slider defaultValue={VALUE_RANGE} min={VALUE_RANGE[0]} max={VALUE_RANGE[1]} step={1} onValueChange={setRange}/>
+			</div>
+		</div>
+	)
+}
 
 
 const LocationAggregatorMap = () => {
+	const [filter, setFilter] = useState(VALUE_RANGE)
+	
+	const dataFilter = new DataFilterExtension({
+		filterSize: 1,
+		// Enable for higher precision, e.g. 1 second granularity
+		// See DataFilterExtension documentation for how to pick precision
+		fp64: false,
+	})
+	
+	const scatterLayer = new ScatterplotLayer<{ coordinates: number[] }>({
+		id: 'scatterplot-layer',
+		data,
+		opacity: 0.8,
+		stroked: true,
+		filled: true,
+		radiusScale: 1,
+		radiusMinPixels: 10,
+		radiusMaxPixels: 100,
+		lineWidthMinPixels: 1,
+		getPosition: d => d.coordinates,
+		getRadius: d => d.v1,
+		getFillColor: d => [255, 0, 0],
+		getLineColor: d => [0, 0, 0],
+		
+		getFilterValue: d => d.v1,
+		filterRange: [filter[0], filter[1]],
+		extensions: [
+			dataFilter,
+		],
+		
+		pickable: true,
+	})
+	
+	// console.log({ filter })
 	
 	
 	return (
 		<div>
 			<DeckGL
 				layers={[
-					// scatterLayer,
-					iconLayer,
+					scatterLayer,
+					// iconLayer,
 				]}
 				initialViewState={INITIAL_VIEW_STATE}
 				controller={{ dragRotate: false }}
@@ -33,7 +86,6 @@ const LocationAggregatorMap = () => {
 					reuseMaps
 					mapStyle={mapBox.style}
 					preventStyleDiffing={true}
-				
 				>
 					<DrawControl
 						position="top-left"
@@ -49,6 +101,8 @@ const LocationAggregatorMap = () => {
 				</Map>
 			
 			</DeckGL>
+			
+			<RangeInput setRange={setFilter}/>
 		</div>
 	)
 }
--- src/components/deck.gl/layers/scatter.layer.tsx
+++ src/components/deck.gl/layers/scatter.layer.tsx
@@ -1,19 +1,4 @@
-import { ScatterplotLayer } from '@deck.gl/layers/typed'
-import data from '@/../../../../data/coordinates.json'
+import { ScatterplotLayer } from '@deck.gl/layers'
+import data from '@/../data/table.json'
+import { DataFilterExtension } from '@deck.gl/extensions'
 
-export const scatterLayer = new ScatterplotLayer({
-	id: 'scatterplot-layer',
-	data,
-	pickable: true,
-	opacity: 0.8,
-	stroked: true,
-	filled: true,
-	radiusScale: 1,
-	radiusMinPixels: 10,
-	radiusMaxPixels: 100,
-	lineWidthMinPixels: 1,
-	getPosition: d => d.coordinates,
-	getRadius: d => d.size,
-	getFillColor: d => [255, 0, 0],
-	getLineColor: d => [0, 0, 0],
-})
