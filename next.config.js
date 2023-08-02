907dc0be1811d4909e6410f944208214c6856f26
markshawn2020 <shawninjuly@gmail.com>
2023-08-02 22:59:48 +0800

    fixed duplication

--- next.config.js
+++ next.config.js
@@ -4,5 +4,9 @@ module.exports = {
 		// esmExternals: 'loose', // ref: https://nextjs.org/docs/messages/import-esm-externals
 	},
 	reactStrictMode: true,
+	
+	webpack: (config, context) => ({
+		...config
+	})
 }
 
--- src/components/react-map-gl/index2.tsx
+++ src/components/react-map-gl/index2.tsx
@@ -1,26 +1,66 @@
-import Map, { Layer, MapRef, Marker, NavigationControl, Source } from 'react-map-gl'
+import ReactMapGl, { Layer, MapRef, Marker, NavigationControl, Source } from 'react-map-gl'
 import { circleLayerProps, INITIAL_VIEW_STATE, MAP_PROJECTION, MapStyle, sourceId, sourceProps, textLayerProps } from '@/config'
-import { useRef, useState } from 'react'
+import { RefObject, useEffect, useRef, useState } from 'react'
 import { ICluster, IProperties } from '@/ds'
 import { CreateDonutChart } from '@/components/react-map-gl/utils'
 import { LanguageControl } from '@/components/deck.gl/controls/language.control'
 
-import 'mapbox-gl/dist/mapbox-gl.css';
+import 'mapbox-gl/dist/mapbox-gl.css'
+import { usePrevious } from '@radix-ui/react-use-previous'
+import mapboxgl from 'mapbox-gl'
+import { create } from 'zustand'
+import { combine } from 'zustand/middleware'
+import { produce } from 'immer'
+import _ from 'lodash'
 
+const masks = new Map<number, RefObject<mapboxgl.Marker>>()
+
+export const useMarkersBear = create(combine<{ markers: Record<number, RefObject<mapboxgl.Marker | null>> }>({
+	markers: {},
+}, (set) => ({
+	addMarker: (id: number, marker: RefObject<mapboxgl.Marker>) => {
+		set(produce((state) => {state.markers[id] = marker}))
+	},
+	delMarker: (id: number) => {
+		set(produce((state) => {state.markers.delete(id)}))
+	},
+})))
+
+export const DynamicMarker = ({ cluster, clusters }: {
+	cluster: ICluster<IProperties>
+	clusters: ICluster<IProperties>[]
+}) => {
+	const ref = useRef<mapboxgl.Marker | null>(null)
+	
+	const { addMarker } = useMarkersBear()
+	useEffect(() => {
+		// addMarker(cluster.id, ref)
+		masks[cluster.id] = ref
+	}, [])
+	
+	return (
+		<Marker
+			ref={ref}
+			key={cluster.id}
+			longitude={cluster.geometry.coordinates[0]}
+			latitude={cluster.geometry.coordinates[1]}
+		>
+			<CreateDonutChart cluster={cluster}/>
+		</Marker>
+	)
+}
 
 export const Index2 = () => {
 	
 	const [clusters, setClusters] = useState<ICluster<IProperties>[]>([])
+	const previousClusters = usePrevious(clusters)
+	const { delMarker } = useMarkersBear()
 	
 	const ref = useRef<MapRef | null>(null)
 	
-	console.log(clusters.map((cluster) => ({
-		id: cluster.id,
-		pos: JSON.stringify(cluster.geometry.coordinates),
-	})))
-	
 	return (
-		<Map
+		<ReactMapGl
+			ref={ref}
 			projection={MAP_PROJECTION}
 			initialViewState={INITIAL_VIEW_STATE}
 			mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN}
@@ -28,11 +68,19 @@ export const Index2 = () => {
 			reuseMaps
 			onRender={() => {
 				if (!ref.current?.isSourceLoaded(sourceId)) return
-				const newClusters = ((ref.current?.querySourceFeatures(sourceId) ?? []) as ICluster<IProperties>[])
-				const validNewClusters = newClusters.filter((item) => item.properties.cluster)
-				setClusters(validNewClusters)
+				const newClusters = _.uniqBy(
+					_.sortBy(((ref.current?.querySourceFeatures(sourceId) ?? []) as ICluster<IProperties>[]), 'id')
+						.filter((item) => item.properties.cluster),
+					'id')
+				const toRemoveClusters = previousClusters
+					.filter((c) => !newClusters.find((cc) => cc.id === c.id))
+				console.log({ previousClusters, clusters, toRemoveClusters })
+				toRemoveClusters.forEach((c) => {
+					masks.get(c.id)?.current.remove()
+					masks.delete(c.id)
+				})
+				setClusters(newClusters)
 			}}
-			ref={ref}
 		>
 			
 			<Source {...sourceProps}>
@@ -44,20 +92,14 @@ export const Index2 = () => {
 			</Source>
 			
 			{
-				clusters?.map((cluster) =>
-					<Marker
-						key={cluster.id}
-						longitude={cluster.geometry.coordinates[0]}
-						latitude={cluster.geometry.coordinates[1]}
-					>
-						<CreateDonutChart cluster={cluster}/>
-					</Marker>,
+				clusters.map((cluster) =>
+					<DynamicMarker cluster={cluster} clusters={clusters} key={cluster.id}/>,
 				)
 			}
 			
 			<NavigationControl/>
 			<LanguageControl/>
-		</Map>
+		</ReactMapGl>
 	)
 }
 
