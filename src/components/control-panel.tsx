1ccdab907471c154b4bbdcb00b70b9a78c4fcb12
markshawn2020 <shawninjuly@gmail.com>
2023-08-05 00:31:04 +0800

    added icons

--- src/components/control-panel.tsx
+++ src/components/control-panel.tsx
@@ -11,6 +11,7 @@ import { useReadXlsx } from '@/hooks/use-read-xlsx'
 import { Switch } from '@/components/ui/switch'
 import { Row } from '@/ds'
 import { useFeatures } from '@/hooks/use-features'
+import { IconBoxMultiple1, IconBoxMultiple2, IconBoxMultiple3 } from '@tabler/icons-react'
 
 
 export const ControlPanel = () => {
@@ -26,10 +27,10 @@ export const ControlPanel = () => {
 	console.log({ fileName, sheetName, cols, rows, map })
 	
 	return (
-		<div id={'control-panel'} className={'w-[360px] whitespace-nowrap shrink-0 h-full p-4 | flex flex-col gap-4'}>
+		<div id={'control-panel'} className={'w-[360px] h-full overflow-y-auto |  whitespace-nowrap shrink-0 p-4 | flex flex-col gap-4'}>
 			
-			<div className={'flex flex-col gap-2'}>
-				<div className={'text-2xl'}>数据输入</div>
+			<div className={'flex flex-col gap-2 '}>
+				<div className={'text-2xl inline-flex items-center gap-2'}><IconBoxMultiple1 className={'text-green-500'}/>数据输入</div>
 				<div className="flex items-center gap-2">
 					<Label htmlFor="xlsx">跳过开头空行数</Label>
 					<Input type={'number'} defaultValue={skipRows} onBlur={(event) => setSkipRows(parseInt(event.currentTarget.value))}/>
@@ -65,20 +66,16 @@ export const ControlPanel = () => {
 					</>
 				)}
 				
-				{!!cols?.length && <DataGrid<Row> className={'w-full'} columns={cols} rows={rows} onRowsChange={setRows}/>}
+				{!!cols?.length && <DataGrid<Row> columns={cols} rows={rows} onRowsChange={setRows}/>}
 			</div>
 			
 			<Separator/>
 			<div className={'flex flex-col gap-2'}>
-				<div className={'text-2xl'}>数据操作</div>
+				<div className={'text-2xl inline-flex items-center gap-2'}><IconBoxMultiple2 className={'text-indigo-500'}/>数据操作</div>
+				
 				
 				<div className={'flex items-center gap-2'}>
-					<Label>启用聚类模式</Label>
-					<Switch checked={clusterMode} onCheckedChange={setClusterMode}/>
-				</div>
-				
-				<div className={'flex items-center gap-2'}>
-					<Label>有效坐标个数： {features.length} / {rows.length}</Label>
+					<Label>指定坐标点列指标（{features.length} / {rows.length}）</Label>
 					<Select value={colIndex.lnglat?.toString()} onValueChange={(v) => setColIndex('lnglat', parseInt(v))}>
 						<SelectTrigger>
 							<SelectValue placeholder={selectColTitle}/>
@@ -88,6 +85,8 @@ export const ControlPanel = () => {
 						</SelectContent>
 					</Select>
 				</div>
+				<p className={'text-muted-foreground text-sm whitespace-normal'}>该列为坐标点，基于格式匹配，可决定有多少圈圈</p>
+				
 				
 				<div className={'flex items-center gap-2'}>
 					<Label>指定可视化列指标</Label>
@@ -100,12 +99,33 @@ export const ControlPanel = () => {
 						</SelectContent>
 					</Select>
 				</div>
+				<p className={'text-muted-foreground text-sm whitespace-normal'}>该列为目标数值，可决定圈圈的大小（其中数值列为百分比大小，非数值列为等宽）</p>
+				
+				<div className={'flex items-center gap-2'}>
+					<Label>指定分类列指标</Label>
+					<Select value={colIndex.category?.toString()} onValueChange={(v) => setColIndex('category', parseInt(v))}>
+						<SelectTrigger>
+							<SelectValue placeholder={selectColTitle}/>
+						</SelectTrigger>
+						<SelectContent>
+							{cols.map((v) => <SelectItem key={v.key} value={v.key}>{v.name}</SelectItem>)}
+						</SelectContent>
+					</Select>
+				</div>
+				<p className={'text-muted-foreground text-sm whitespace-normal'}>该列为分类，可决定圈圈的分层颜色</p>
+				
+				<div className={'flex items-center gap-2'}>
+					<Label>启用聚类模式</Label>
+					<Switch checked={clusterMode} onCheckedChange={setClusterMode}/>
+				</div>
+				<p className={'text-muted-foreground text-sm whitespace-normal'}>启用后将基于坐标距离自动聚类，只显示该 zoom 放缩级别下的单元</p>
+			
 			
 			</div>
 			
 			<Separator/>
 			<div className={'flex flex-col gap-2'}>
-				<div className={'text-2xl'}>UI</div>
+				<div className={'text-2xl inline-flex items-center gap-2'}><IconBoxMultiple3 className={'text-pink-500'}/>UI</div>
 				
 				
 				<div className={'flex items-center gap-2'}>
